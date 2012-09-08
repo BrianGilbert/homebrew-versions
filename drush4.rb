18e6a847e372bfd02806d5e85a681cc9c32874ea
Brian Gilbert <brian@realityloop.com>
2012-09-08 15:36:54 +1000

    renamed to drush4.rb

--- drush4.rb
+++ drush4.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Drush46 < Formula
+class Drush4 < Formula
   homepage 'http://drupal.org/project/drush'
   url 'http://ftp.drupal.org/files/projects/drush-7.x-4.6.tar.gz'
   md5 '2cf1c382f44f0c0ee0af6b62587a22a6'
